de09566a16bbf4719640f51e7d7b5bff9bd5af50
Bruno Romero de Azevedo <bruno.azevedo@lge.com>
2018-01-24 14:31:02 -0200

    Add missing GlideModule class

---
+++ app/src/main/java/br/brunodea/nevertoolate/util/NeverTooLateGlideModule.java
@@ -0,0 +1,7 @@
+package br.brunodea.nevertoolate.util;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public class NeverTooLateGlideModule extends AppGlideModule {}
